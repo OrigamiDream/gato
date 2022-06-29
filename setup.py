565f25a611fc3226aefe0bd3e7291ed3a15e434a
OrigamiDream <sdy36071@naver.com>
2022-06-29 11:41:12 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+setup(name='gato',
+      version='0.0.1',
+      description='Unofficial Gato: A Generalist Agent',
+      url='https://github.com/OrigamiDream/gato.git',
+      author='OrigamiDream',
+      packages=find_packages(),
+      zip_safe=False,
+      include_package_data=True,
+      install_requires=[
+          'tensorflow>=2.0',
+          'numpy'
+      ])
