3ada0e70cd1aa811a3ac9a13a74be345e9525de9
OrigamiDream <hello@origamidream.me>
2023-05-27 03:25:39 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,31 @@ from setuptools import find_packages, setup
 
 setup(
     name='gato-tf',
-    version='0.0.3',
+    version='0.0.4',
     description='Unofficial Gato: A Generalist Agent',
     url='https://github.com/OrigamiDream/gato.git',
     author='OrigamiDream',
-    author_email='sdy36071@naver.com',
+    author_email='hello@origamidream.me',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     license='MIT',
-    packages=find_packages(),
+    packages=find_packages(exclude=[]),
     install_requires=[
         'tensorflow>=2.11',
         'flowchain>=0.0.4'
     ],
+    python_requires='>=3.10.0',
     keywords=[
         'deep learning',
         'gato',
         'tensorflow',
         'generalist agent'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10'
     ]
 )
